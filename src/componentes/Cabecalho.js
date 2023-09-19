83f6996f9ebc2fad7e23567f56a65a22d1f06f4c
Bruno <bruno@pop-os.localdomain>
2023-09-19 20:43:21 -0300

    Aula 02 react - devweb01

---
+++ src/componentes/Cabecalho.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Cabecalho({titulo}) {
+  return (
+    <div>
+      <h1>{titulo}</h1>
+    </div>
+  )
+}
---
+++ src/componentes/CarrinhoDeCompras.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+export default function CarrinhoDeCompras({itens}) {
+  const renderItem = (i) => {
+    return (<li>{i.nome} - R$ {i.valor.toFixed(2)}</li>)
+  };
+
+  const calculaTotal = () => {
+    let total = 0.0;
+    itens.forEach(i => total += i.valor);
+    return total;
+  };
+
+  return (
+    <>
+      <h3>Carrinho de Compras:</h3>
+      <ol>
+        {itens.map(renderItem)}
+      </ol>
+      <h3>Total das compras: R$ {calculaTotal().toFixed(2)}</h3>
+    </>
+  )
+}
